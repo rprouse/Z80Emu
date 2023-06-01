a1dd047a0f2273206102ea8eb950dc2a16a201e6
Rob Prouse <r.prouse@valsoftcorp.com>
2023-06-01 11:17:22 -0400

    Remove DMA

--- Z80Emu.Core/Emulator.cs
+++ Z80Emu.Core/Emulator.cs
@@ -7,24 +7,19 @@ public class Emulator
 {
     public CPU _cpu;
     public MMU _mmu;
-    public Dma _dma;
     public Interupts _int;
 
     // constructor
     public Emulator()
     {
         _mmu = new MMU();
-
-        _dma = new Dma(_mmu);
         _int = new Interupts(_mmu);
-
         _cpu = new CPU(_int, _mmu);
     }
 
     public int Tick()
     {
         _cpu.Tick();
-        _dma.Tick();
         return 1;
     }
 }
