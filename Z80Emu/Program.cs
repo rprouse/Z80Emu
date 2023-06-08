d36b535343817b72fd6f027fa51f3533757b2de4
Rob Prouse <r.prouse@valsoftcorp.com>
2023-06-08 14:44:19 -0400

    Improving the console UI

--- Z80Emu/Program.cs
+++ Z80Emu/Program.cs
@@ -76,11 +76,11 @@ while(true)
 static void ViewRegisters(Emulator emulator)
 {
     var r = emulator.CPU.Registers;
-    AnsiConsole.Markup($"[blue]AF [/][aqua]{r.AF:X4}[/] [blue]BC [/][aqua]{r.BC:X4}[/] [blue]DE [/][aqua]{r.DE:X4}[/] [blue]HL [/][aqua]{r.HL:X4}[/]");
-    AnsiConsole.MarkupLine($"    [blue]SP [/][aqua]{r.SP:X4}[/] [blue]PC [/][aqua]{r.PC:X4}[/]");
-    AnsiConsole.Markup($"[blue]AF`[/][aqua]{r.AF_:X4}[/] [blue]BC`[/][aqua]{r.BC_:X4}[/] [blue]DE`[/][aqua]{r.DE_:X4}[/] [blue]HL`[/][aqua]{r.HL_:X4}[/]");
-    AnsiConsole.MarkupLine($"    [blue]SP`[/][aqua]{r.SP_:X4}[/] [blue]PC`[/][aqua]{r.PC_:X4}[/]");
-    AnsiConsole.MarkupLine($"[blue]S:[/][aqua]{(r.FlagS ? '1' : '0')}[/] [blue]Z:[/][aqua]{(r.FlagZ ? '1' : '0')}[/] [blue]H:[/][aqua]{(r.FlagH ? '1' : '0')}[/] [blue]PV:[/][aqua]{(r.FlagPV ? '1' : '0')}[/] [blue]N:[/][aqua]{(r.FlagN ? '1' : '0')}[/] [blue]C:[/][aqua]{(r.FlagC ? '1' : '0')}[/]");
+    AnsiConsole.Markup($"[blue]AF [/][aqua]{r.AF:X4}[/] [blue]BC [/][aqua]{r.BC:X4}[/] [blue]DE [/][aqua]{r.DE:X4}[/] [blue]HL [/][aqua]{r.HL:X4}[/] [blue]IX [/][aqua]{r.IX:X4}[/]");
+    AnsiConsole.Markup($"   [blue]SP [/][aqua]{r.SP:X4}[/] [blue]PC [/][aqua]{r.PC:X4}[/]");
+    AnsiConsole.MarkupLine($"   [maroon]S:[/][aqua]{(r.FlagS ? '1' : '0')}[/] [maroon]Z:[/][aqua]{(r.FlagZ ? '1' : '0')}[/] [maroon]H:[/][aqua]{(r.FlagH ? '1' : '0')}[/] [maroon]P/V:[/][aqua]{(r.FlagPV ? '1' : '0')}[/] [maroon]N:[/][aqua]{(r.FlagN ? '1' : '0')}[/] [maroon]C:[/][aqua]{(r.FlagC ? '1' : '0')}[/]");
+    AnsiConsole.Markup($"[blue]AF'[/][aqua]{r.AF_:X4}[/] [blue]BC'[/][aqua]{r.BC_:X4}[/] [blue]DE'[/][aqua]{r.DE_:X4}[/] [blue]HL'[/][aqua]{r.HL_:X4}[/] [blue]IX [/][aqua]{r.IY:X4}[/]");
+    AnsiConsole.MarkupLine($"   [blue]SP'[/][aqua]{r.SP_:X4}[/] [blue]PC'[/][aqua]{r.PC_:X4}[/]");
     AnsiConsole.WriteLine();
 }
 
@@ -89,10 +89,10 @@ static void ViewMemory(Emulator emulator, word startAddr = 0x0100, word len = 0x
     startAddr = (word)(startAddr / 0xF * 0xF + 1);
     for (word addr = startAddr; addr < startAddr + len; addr += 0xF)
     {
-        AnsiConsole.Markup($"[cyan]{addr:X4}[/] ");
+        AnsiConsole.Markup($"[blue]{addr:X4}[/] ");
         for (word i = 0; i < 0xF; i++)
         {
-            AnsiConsole.Markup($"[silver]{emulator.Memory[addr + i]:X2}[/] ");
+            AnsiConsole.Markup($"[aqua]{emulator.Memory[addr + i]:X2}[/] ");
         }
         for (word i = 0; i < 0xF; i++)
         {
@@ -114,18 +114,32 @@ static void ViewDisassembly(Emulator emulator)
 
     while (addr < startAddr + len)
     {
-        AnsiConsole.Markup($"[cyan]{addr:X4}[/] ");
+        AnsiConsole.Markup($"[blue]{addr:X4}[/] ");
         try
         {
             Opcode opcode = emulator.Disassemble(addr);
-            addr += opcode.Length;
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i < opcode.Length)
+                    AnsiConsole.Markup($"[aqua]{emulator.Memory[addr + i]:X2}[/] ");
+                else
+                    AnsiConsole.Markup($"   ");
+            }
+
             AnsiConsole.Markup($"[silver]{opcode.Mnemonic}[/]");
-            AnsiConsole.MarkupLine($"\t[green]; {opcode.Description}[/]");
+
+            if (opcode.Mnemonic != "NOP")
+                AnsiConsole.MarkupLine($"\t[green]; {opcode.Description}[/]");
+            else
+                AnsiConsole.WriteLine();
+
+            addr += opcode.Length;
 
         }
         catch (Exception)
         {
-            AnsiConsole.MarkupLine($"[silver]{emulator.Memory[addr]:X2}[/]");
+            AnsiConsole.MarkupLine($"[aqua]{emulator.Memory[addr]:X2}[/]");
             addr++;
         }
     }
