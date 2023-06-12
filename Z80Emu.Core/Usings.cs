24c16711e251a0eb63a9699a3745d20e73e0662c
Rob Prouse <r.prouse@valsoftcorp.com>
2023-06-12 16:44:15 -0400

    Fixing build warnings and CI/CD

--- Z80Emu.Core/Usings.cs
+++ Z80Emu.Core/Usings.cs
@@ -1,1 +1,4 @@
+// The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+#pragma warning disable CS8981
 global using word = System.UInt16;
+#pragma warning restore CS8981
--- Z80Emu.Tests/Usings.cs
+++ Z80Emu.Tests/Usings.cs
@@ -1,4 +1,7 @@
 global using NUnit.Framework;
 global using FluentAssertions;
 
+// The type name only contains lower-cased ascii characters. Such names may become reserved for the language.
+#pragma warning disable CS8981
 global using word = System.UInt16;
+#pragma warning restore CS8981
