20e3b64a90efb5573fc3f203251c1177b1a9cd6d
Rob Prouse <rob@prouse.org>
2023-08-21 22:59:57 -0400

    Allow 0x and $ when parsing HEX values

---
+++ Z80Emu.Core/Utilities/WordExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace Z80Emu.Core;
+
+public static class WordExtensions
+{
+    public static word ParseHex(this string s) =>
+        word.Parse(StripHexIdentifiers(s), NumberStyles.HexNumber, null);
+
+    public static bool TryParseHex(this string s, out word hex) =>
+        word.TryParse(StripHexIdentifiers(s), NumberStyles.HexNumber, null, out hex);
+
+    private static string StripHexIdentifiers(string s)
+    {
+        if (s.StartsWith("0x"))
+            return s.Substring(2);
+
+        if (s.StartsWith("$"))
+            return s.Substring(1);
+
+        return s;
+    }
+}
